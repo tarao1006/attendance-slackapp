03a7a10ccbb713855bb9e71e262572ba470578bd
tarao1006 <taiga4562@gmail.com>
2020-12-01 00:19:28 +0900

    Update main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tarao1006/attendance-slackapp
 
 go 1.14
+
+require github.com/slack-go/slack v0.7.2
--- main.go
+++ main.go
@@ -1,10 +1,105 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+
+	"github.com/slack-go/slack"
 )
+
+func generateModalRequest() slack.ModalViewRequest {
+	titleText := slack.NewTextBlockObject("plain_text", "My App", false, false)
+	closeText := slack.NewTextBlockObject("plain_text", "Close", false, false)
+	submitText := slack.NewTextBlockObject("plain_text", "Submit", false, false)
+
+	headerText := slack.NewTextBlockObject("mrkdwn", "Please enter your name", false, false)
+	headerSection := slack.NewSectionBlock(headerText, nil, nil)
+
+	firstNameText := slack.NewTextBlockObject("plain_text", "First Name", false, false)
+	firstNamePlaceholder := slack.NewTextBlockObject("plain_text", "Enter your first name", false, false)
+	firstNameElement := slack.NewPlainTextInputBlockElement(firstNamePlaceholder, "firstName")
+
+	firstName := slack.NewInputBlock("First Name", firstNameText, firstNameElement)
+
+	lastNameText := slack.NewTextBlockObject("plain_text", "Last Name", false, false)
+	lastNamePlaceholder := slack.NewTextBlockObject("plain_text", "Enter your first name", false, false)
+	lastNameElement := slack.NewPlainTextInputBlockElement(lastNamePlaceholder, "lastName")
+	lastName := slack.NewInputBlock("Last Name", lastNameText, lastNameElement)
+
+	blocks := slack.Blocks{
+		BlockSet: []slack.Block{
+			headerSection,
+			firstName,
+			lastName,
+		},
+	}
+
+	var modalRequest slack.ModalViewRequest
+	modalRequest.Type = slack.ViewType("modal")
+	modalRequest.Title = titleText
+	modalRequest.Close = closeText
+	modalRequest.Submit = submitText
+	modalRequest.Blocks = blocks
+	return modalRequest
+}
+
+func verifySigningSecret(r *http.Request) error {
+	signingSecret := os.Getenv("SIGNING_SECRET")
+	verifier, err := slack.NewSecretsVerifier(r.Header, signingSecret)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+	verifier.Write(body)
+	if err = verifier.Ensure(); err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func handleSlash(w http.ResponseWriter, r *http.Request) {
+
+	err := verifySigningSecret(r)
+	if err != nil {
+		fmt.Printf(err.Error())
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	s, err := slack.SlashCommandParse(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err.Error())
+		return
+	}
+
+	switch s.Command {
+	case "/test":
+		api := slack.New(os.Getenv("OAUTH_ACCESS_TOKEN"))
+		modalRequest := generateModalRequest()
+		_, err = api.OpenView(s.TriggerID, modalRequest)
+		if err != nil {
+			fmt.Printf("Error opening view: %s", err)
+		}
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!")
@@ -13,5 +108,6 @@ func hello(w http.ResponseWriter, r *http.Request) {
 func main() {
 	port := os.Getenv("PORT")
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/test", handleSlash)
 	http.ListenAndServe(":"+port, nil)
 }
