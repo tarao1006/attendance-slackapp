a68dcdc291512bfb02416a8b18fd9c4867887a08
tarao1006 <taiga4562@gmail.com>
2020-12-03 23:42:02 +0900

    Update error

--- middleware/slackapp.go
+++ middleware/slackapp.go
@@ -2,8 +2,8 @@ package middleware
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
@@ -14,22 +14,30 @@ func VerifyingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		verifier, err := slack.NewSecretsVerifier(r.Header, os.Getenv("SIGNING_SECRET"))
 		if err != nil {
-			fmt.Println(err.Error())
+			log.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			fmt.Println(err.Error())
+			log.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
 		r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 
 		if _, err := verifier.Write(body); err != nil {
-			fmt.Println(err.Error())
+			log.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
 		if err := verifier.Ensure(); err != nil {
-			fmt.Println(err.Error())
+			log.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
 		next.ServeHTTP(w, r)
