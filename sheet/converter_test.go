f5e128acc26f34aa8639c20d26bc77847571c4e8
tarao1006 <taiga4562@gmail.com>
2020-12-06 01:07:33 +0900

    Add converter_test

---
+++ sheet/converter_test.go
@@ -0,0 +1,62 @@
+package sheet
+
+import (
+	"testing"
+	"time"
+)
+
+func TestConverter_GetColumnNumber(t *testing.T) {
+	converter := NewConverter()
+	tests := []struct {
+		Date     string
+		Expected int64
+	}{
+		{
+			"2020-09-28",
+			129,
+		},
+		{
+			"2020-12-30",
+			222,
+		},
+		{
+			"2021-01-01",
+			224,
+		},
+	}
+
+	for _, tt := range tests {
+		result := converter.GetColumnNumber(tt.Date)
+		if result != tt.Expected {
+			t.Errorf("expected: %d, result: %d", tt.Expected, result)
+		}
+	}
+}
+
+func TestConverter_GetDate(t *testing.T) {
+	converter := NewConverter()
+	tests := []struct {
+		Num      int64
+		Expected time.Time
+	}{
+		{
+			129,
+			time.Date(2020, time.September, 28, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			222,
+			time.Date(2020, time.December, 30, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			224,
+			time.Date(2021, time.January, 1, 0, 0, 0, 0, time.UTC),
+		},
+	}
+
+	for _, tt := range tests {
+		result := converter.GetDate(tt.Num)
+		if result != tt.Expected {
+			t.Errorf("expected: %v, result: %v", tt.Expected, result)
+		}
+	}
+}
